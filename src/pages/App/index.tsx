9f7e445f7314779e8e8582608bdd39abf3041487
Lucas Rachid Martins <softfocus@nextage.com.br>
2023-03-10 10:57:07 -0300

    Atualizacoes

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import './index.scss';
 import logo from './../../assets/img/logo.svg';
+import "./index.scss";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+
+  function SearchForData(){
+    useEffect(() => {
+      axios("https://digimon-api.vercel.app/api/digimon")
+        .then((response) => {
+          setData(response.data);
+          setError(null);
+          console.log(response.data);
+        })
+        .catch(setError);
+    }, []);
+
+    if (error) return error;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div>
+    //   {data.map(item => (
+    //     <div key={item}>
+    //       {/* <h2>{item.title}</h2>
+    //       <p>{item.description}</p> */}
+    //       <p>{item.name}</p>
+    //     </div>
+    //   ))}
+    // </div>
+    <div>
+      <button onClick={SearchForData()}>Buscar Dados</button>
     </div>
   );
 }
